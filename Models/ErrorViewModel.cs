5a77643c017c3fb810322fc4e16d5d58cf10f843
nhanlt@irismedia.vn <khongten1452@gmail.com>
2021-12-10 11:55:45 +0700

    setup

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace net_core_restful_api_CICD.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
